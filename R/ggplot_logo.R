0d87fc452d9112781b0f3a8942baa24ce0e6f1a6
Andreas Wyss <78662247+socialthink@users.noreply.github.com>
2023-01-15 22:37:43 +0100

    create

---
+++ R/ggplot_logo.R
@@ -0,0 +1,14 @@
+#' Add together two numbers
+#'
+#' @param p a ggplot
+#' @param img path to an png-file
+#' @returns The sum of `x` and `y`.
+#' @export
+#' @examples
+#' add(1, 1)
+ggplot_logo <- function(p, logo=system.file("extdata", "logo.png", package = "sothi")) {
+  img <- image_read(logo)
+  ggdraw() +
+  draw_plot(p,x = 0, y = 0, width = 1, height = 1) +
+  draw_image(img,x = 0.01, y = 0.02, width = 0.1, height = 0.1)
+}
